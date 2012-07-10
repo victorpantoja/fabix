ac2084b91b2b298dbdae9f806d5dca2936dc750a
Rodrigo Machado <rcmachado@gmail.com>
2012-07-10 16:59:44 -0300

    Fix syntax error

--- fabix/system.py
+++ fabix/system.py
@@ -8,7 +8,8 @@ def restart_service(service, force_start=True):
     if force_start:
         upstart_ensure(service)
     else:
-        sudo('service {0} restart').format(service))
+        sudo('service {0} restart'.format(service))
+
 
 @task
 def reload_service(service):
