905920f5c89f6b58dd8b4c42f134b726e1a6081c
paulo alem <biggahed@gmail.com>
2012-10-19 18:28:35 -0300

    crontab commands

---
+++ fabix/system/crontab.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import fabric.api as fab
+import cuisine
+
+
+def install(filename, user="root"):
+    """
+    Installs crontab from a given cronfile
+    """
+    tmp_file = fab.run("mktemp fabixcron")
+    cuisine.file_upload(tmp_file, filename)
+    fab.sudo("crontab -u{} {}".format(user, tmp_file))
