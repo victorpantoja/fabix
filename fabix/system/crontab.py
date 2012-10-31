aa0b61b44e631c3a12a16025e93d7e962de23c2f
Rodrigo Machado <rcmachado@gmail.com>
2012-10-31 14:46:08 -0200

    Remove duplicate lines from cron file without sorting

--- fabix/system/crontab.py
+++ fabix/system/crontab.py
@@ -10,10 +10,7 @@ def install(filename, user="root", append=False):
     new_crontab = fab.run("mktemp fabixcron.XXXX")
     cuisine.file_upload(new_crontab, filename)
     if append is True:
-        sorted_crontab = fab.run("mktemp fabixcron.XXXX")
         # When user have no crontab, then crontab command returns 1 error code
         with fab.settings(warn_only=True):
-            fab.sudo("crontab -u {} -l >> {} 2> /dev/null".format(user, new_crontab))
-        fab.sudo("sort -u -o {} {}".format(sorted_crontab, new_crontab))
-        new_crontab = sorted_crontab
+            fab.sudo("crontab -u {} -l 2> /dev/null | awk '!x[$0]++{{print $0}}' >> {}".format(user, new_crontab))
     fab.sudo("crontab -u {} {}".format(user, new_crontab))
