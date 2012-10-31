be998a9dcc076b3b17b3a1db0247292a51c7b918
Rodrigo Machado <rcmachado@gmail.com>
2012-10-31 14:26:25 -0200

    Add option to append to crontab instead of replace it

--- fabix/system/crontab.py
+++ fabix/system/crontab.py
@@ -3,10 +3,17 @@ import fabric.api as fab
 import cuisine
 
 
-def install(filename, user="root"):
+def install(filename, user="root", append=False):
     """
     Installs crontab from a given cronfile
     """
-    tmp_file = fab.run("mktemp fabixcron.XXXX")
-    cuisine.file_upload(tmp_file, filename)
-    fab.sudo("crontab -u{} {}".format(user, tmp_file))
+    new_crontab = fab.run("mktemp fabixcron.XXXX")
+    cuisine.file_upload(new_crontab, filename)
+    if append is True:
+        sorted_crontab = fab.run("mktemp fabixcron.XXXX")
+        # When user have no crontab, then crontab command returns 1 error code
+        with fab.settings(warn_only=True):
+            fab.sudo("crontab -u {} -l >> {} 2> /dev/null".format(user, new_crontab))
+        fab.sudo("sort -u -o {} {}".format(sorted_crontab, new_crontab))
+        new_crontab = sorted_crontab
+    fab.sudo("crontab -u {} {}".format(user, new_crontab))
