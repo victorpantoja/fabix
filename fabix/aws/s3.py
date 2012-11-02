ae463e2103644025cd0fdff8aa3c95ce10ce0387
Rodrigo Machado <rcmachado@gmail.com>
2012-11-01 22:39:04 -0200

    Fix check if file was already uploaded to S3

--- fabix/aws/s3.py
+++ fabix/aws/s3.py
@@ -1,5 +1,4 @@
 import os
-import hashlib
 
 import boto
 from boto.utils import compute_md5
@@ -13,15 +12,29 @@ def upload_file(bucket, key_name, file_path, remote_prefix=None, policy='public-
 
     if remote_prefix:
         key_name = '{0}/{1}'.format(remote_prefix, key_name)
+
+    fd = open(file_path)
+    md5 = compute_md5(fd)
+    fd.close()
+
+    current_md5 = None
+    current_key = bucket.lookup(key_name)
+    if current_key:
+        current_md5 = current_key.get_metadata('fabix-md5')
+        if current_md5 == md5[0]:
+            for k, v in metadata.iteritems():
+                current_key.set_metadata(k, v)
+            puts("Skip file {0}".format(file_path))
+            return current_key
+
     key = bucket.new_key(key_name)
 
     for k, v in metadata.iteritems():
         key.set_metadata(k, v)
 
-    fd = open(file_path)
-    md5 = compute_md5(fd)
-    fd.close()
     key.set_metadata('fabix-md5', md5[0])
+
+    puts("Upload file {0}".format(file_path))
     key.set_contents_from_filename(file_path, md5=md5, policy=policy)
     return key
 
@@ -31,16 +44,6 @@ def get_key_name(local_path, fullpath):
     l = key_name.split(os.sep)
     key_name = '/'.join(l)
     return key_name.lstrip('/')
-
-
-def hash_file(filename):
-    h = hashlib.md5()
-    fd = open(filename, 'rb')
-    while True:
-        data = fd.read(8 * 1024)
-        h.update(data)
-    fd.close()
-    return h.hexdigest()
 
 
 @task
@@ -54,15 +57,4 @@ def sync_dir_up(bucket_name, local_path, remote_prefix=None, metadata=None):
             file_path = os.path.join(root, fname)
             key_name = get_key_name(local_path, file_path)
 
-            key = bucket.lookup(key_name)
-            if key:
-                key_md5 = key.get_metadata('fabix-md5')
-                fd = open(file_path)
-                md5 = compute_md5(fd)
-                fd.close()
-                if md5[0] == key_md5:
-                    puts("Skipping {0} (MD5 match)".format(file_path))
-                    continue
-
-            puts("Upload file {0}".format(file_path))
             upload_file(bucket, key_name, file_path, remote_prefix=remote_prefix, metadata=metadata)
